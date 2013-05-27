7952a219a6d975bb157ed6880718095b348f846d
liguang <kidlg@foxmail.com>
2013-05-27 14:56:37 +0800

    initialize solution

---
+++ src/SQ/SQTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
